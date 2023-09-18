54779c1cafc4071bc5b364ec6b61cf379dd43363
Pauloper1 <paulcrescent.p@gmail.com>
2023-09-18 14:07:37 +0530

    nmap_scan function added

---
+++ Agent.py
@@ -0,0 +1,80 @@
+import requests
+import time
+import json
+import nmap
+import threading
+
+
+server_url = "http://localhost:3000"
+
+# Create a config file that has ip_range that has to be scanned.
+# Also create function that can edit the config file.
+
+def nmap_scan():
+
+    target = "192.168.0.0-255" # This ip_range is for testing only.
+    nm = nmap.PortScanner()
+
+    hosts = nm.scan(target, arguments= "-sS -O")
+    host_result = []
+    for each_host in hosts['scan']:
+        # [
+        #     {   
+        #         hostname,
+        #         ip_address,
+        #         mac_address,
+        #         os_match,
+        #         status
+        #         Device type,
+        #      },
+        #       {...}
+        # ]
+
+        host = {
+            "hostname": hosts['scan'][each_host]['hostnames'][0]['name'],
+            "ip_address": each_host,
+            "mac_address": hosts['scan'][each_host].get('addresses','').get('mac',''),
+            "os": hosts['scan'][each_host]['osmatch'][0]['name'],
+            "status": hosts['scan'][each_host].get('status','')['state'],
+            "device_type": hosts['scan'][each_host]['hostnames'][0]['type']
+
+        }
+        host_result.append(host)
+        print("Host: \n")
+        print(host)
+    return host_result
+def get_request():
+    while True:
+        try:
+            print("check")
+            response = requests.get(server_url + "/agent-do")
+            print(response.status_code)
+            response = response.text
+
+            # convert the text to dictionary
+            response = json.loads(response)
+            print(response)
+            print(response['work'])
+
+            # scan function
+            if(response['work'] == 'scan'):
+                scan_result = nmap_scan()
+                scan_result = json.dumps(scan_result)
+                print("Scanned result\n" + scan_result)
+                # post request to send the scan_result
+                try:
+                    headers = {'Content-Type': 'application/json'}
+                    response = requests.post(server_url+"/scan_result", data= scan_result, headers= headers)
+                    print(response)
+                except Exception as e:
+                    print("Post request failed "+ str(e))
+            
+        except Exception as e:
+            print(f'{e}')
+        time.sleep(1)     
+
+get_thread = threading.Thread(target=get_request)
+get_thread.start()
+
+# other work
+
